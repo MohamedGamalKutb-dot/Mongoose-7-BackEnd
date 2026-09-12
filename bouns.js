66987c2485d5e456ee2234b2c74702e20f3d6e57
mohamed.g.kutb@gmail.com <mohamed.g.kutb@gmail.com>
2026-09-13 02:40:50 +0300

    Update bouns.js: implement longestCommonPrefix

--- bouns.js
+++ bouns.js
@@ -1,23 +1,18 @@
-var romanToInt = function(s) {
-    const values = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    };
-
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        if (values[s[i]] < values[s[i + 1]]) {
-            result -= values[s[i]];
-        } else {
-            result += values[s[i]];
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = (strs) => {
+    if (!strs.length) return "";
+    
+    let prefix = strs[0];
+    
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
         }
     }
-
-    return result;
+    
+    return prefix;
 };
